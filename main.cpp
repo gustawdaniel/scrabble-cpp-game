3183f44a61d49ec26bec4a7a9d18e63e8c7279a6
gustawdaniel <gustaw.daniel@gmail.com>
2022-12-05 21:11:22 +0400

    lint

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ public:
     void init() {
         initscr();
 
-        for (int i = 2; i < size+ 2; i++) {
-            const char c[] = { static_cast<char>(65 + i - 2), '\0' };
-            mvprintw(0,i, "%s", c);
-            mvprintw(size+1,i, "%s", c);
+        for (int i = 2; i < size + 2; i++) {
+            const char c[] = {static_cast<char>(65 + i - 2), '\0'};
+            mvprintw(0, i, "%s", c);
+            mvprintw(size + 1, i, "%s", c);
         }
-        for (int i = 1; i < size+ 1; i++) {
-            mvprintw(i,0, "%2d", i);
-            mvprintw(i,0+size+2, "%d", i);
+        for (int i = 1; i < size + 1; i++) {
+            mvprintw(i, 0, "%2d", i);
+            mvprintw(i, 0 + size + 2, "%d", i);
         }
     }
 };
@@ -31,7 +31,6 @@ struct Game {
         return 0;
     }
 };
-
 
 
 int main() {
