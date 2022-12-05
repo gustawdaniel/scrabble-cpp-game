7a8c64dcf43462a81511cb7a36888e16559fd9d4
gustawdaniel <gustaw.daniel@gmail.com>
2022-12-05 21:51:14 +0400

    added positions selection

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@ struct Map {
 public:
     int size = 15;
 
-    void init() {
+    void init() const {
         initscr();
+        noecho(); // no print typed characters
+        keypad(stdscr, TRUE); // allow for arrays
 
         for (int i = 2; i < size + 2; i++) {
             const char c[] = {static_cast<char>(65 + i - 2), '\0'};
@@ -19,13 +21,57 @@ public:
     }
 };
 
-struct Game {
+struct Pos {
+    int x;
+    int y;
+};
+
+struct Player {
+    Pos pos;
+
     void init() {
-        Map *map = new Map();
-        map->init();
+        pos.x = 2;
+        pos.y = 1;
+        mvprintw(pos.y, pos.x, "");
     }
 
-    int end() {
+    void setPosition() {
+        while (int c = getch()) {
+            switch (c) {
+                case KEY_LEFT: {
+                    pos.x--;
+                    break;
+                }
+                case KEY_RIGHT: {
+                    pos.x++;
+                    break;
+                }
+                case KEY_UP: {
+                    pos.y--;
+                    break;
+                }
+                case KEY_DOWN: {
+                    pos.y++;
+                    break;
+                }
+                default: {}
+            }
+            mvprintw(pos.y, pos.x, "");
+        }
+    }
+};
+
+struct Game {
+    static void init() {
+        Map *map = new Map();
+        map->init();
+
+        auto *p1 = new Player();
+        p1->init();
+        p1->setPosition();
+    }
+
+    static int end() {
         getch();
         endwin();
         return 0;
@@ -34,9 +80,8 @@ struct Game {
 
 
 int main() {
-    Game *g = new Game();
-    g->init();
-    return g->end();
+    Game::init();
+    return Game::end();
 }
 
 
